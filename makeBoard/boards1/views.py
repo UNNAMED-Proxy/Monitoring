36f9ec9730eee5435c0a75c5b05c9a908adcd94c
kengekeng <tidlxod1@naver.com>
2024-01-23 13:08:21 +0900

    Changes
    Signed-off-by: kengekeng <tidlxod1@naver.com>

--- makeBoard/boards1/views.py
+++ makeBoard/boards1/views.py
@@ -1,8 +1,9 @@
 # 사용자정의
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # 렌더링
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Topic, Reply
 # 모델의 클래스명
@@ -15,10 +16,6 @@ def home(request):
     return render(request, 'home.html', {'topics': topics})
 
 
-# 위 내용과 같다.
-
-
-# 추가하기
 def new_topic(request):
     topics = Topic.objects.all()
     if request.method == 'POST':
@@ -33,12 +30,17 @@ def new_topic(request):
             writter=user
         )
 
-        post = Reply.objects.create(
-            message=message,
-            created_by=user
-
-        )
+        # post = Reply.objects.create(
+        #     message=message,
+        #     created_by=user
+        #
+        # )
 
         return redirect('home')
 
     return render(request, 'new_topic.html', {'topics': topics})
+
+def answer_create(request, topick_id):
+    topic = get_object_or_404(Topic, pk=topick_id)
+    topic.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('new_topic.html', topick_id=topic.id)
